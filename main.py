b26cbbcc6fbd7d7ef532f9d027309ca741de42c8
BerryFarm97 <ab.granbery@gmail.com>
2025-07-30 00:43:39 -0500

    Add delete functionality and alphabetical sorting

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def initialize_db():
     conn.commit()
     conn.close()
 
-def create_job():
+def create_job():                     #creates new job listings
     comp_name = input("Company Name: ")
     job_title = input("Position Name: ")
     status = input("Status of Application: ")
@@ -27,31 +27,46 @@ def create_job():
     conn.close()
 
 
-def view_all_jobs():
+def view_all_jobs():                       #list saved jobs
     conn = sqlite3.connect("jobs.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM jobs")
+    cursor.execute("SELECT * FROM jobs ORDER BY company ASC")  #Adjust this to sort jobs alphabetically
     results = cursor.fetchall()
     for row in results:
         print(f"[{row[0]}] {row[1]} - {row[2]} - {row[3]} - {row[4]}")
     conn.close()
 
-def show_menu():
+def show_menu():                 #Add a menu for easy choice selection
     print("1. Add a new job")
     print("2. View saved jobs")
-    print("3. Exit")
+    print("3. Remove job")
+    print("4. Exit")
+
+def delete_job():                 #lets you remove listings if no longer needed
+    print("\nCurrent Job Applications:")   #Added this to make it smoother for user when trying to remove a job listing
+    view_all_jobs()
+    print("Which jobs would you like to remove?")
+    job_id = input("Enter the ID of the job you want to delete: ")
+    
+    conn = sqlite3.connect("jobs.db")
+    conn.execute("DELETE FROM jobs WHERE id = ?", (job_id))
+    conn.commit()
+    conn.close()
+    print(f"Job with ID {job_id} has been deleted.")
 
 def main():
     initialize_db()
     while True:
         show_menu()
-        choice = input("Choose a option (1-3): ")
+        choice = input("Choose a option (1-4): ")
 
         if choice == '1':
             create_job()
         elif choice == '2':
             view_all_jobs()
         elif choice == '3':
+            delete_job()
+        elif choice == '4':
             break
         else:
             print("Invalid options. Please choose a valid option.")
