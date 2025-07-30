21b4670444e056dfb8570cf2c616558ac5f04b56
BerryFarm97 <ab.granbery@gmail.com>
2025-07-30 00:51:12 -0500

    Add ability to update the status of job applications

--- main.py
+++ main.py
@@ -40,7 +40,8 @@ def show_menu():                 #Add a menu for easy choice selection
     print("1. Add a new job")
     print("2. View saved jobs")
     print("3. Remove job")
-    print("4. Exit")
+    print("4. Update Status")
+    print("5. Exit")
 
 def delete_job():                 #lets you remove listings if no longer needed
     print("\nCurrent Job Applications:")   #Added this to make it smoother for user when trying to remove a job listing
@@ -54,11 +55,24 @@ def delete_job():                 #lets you remove listings if no longer needed
     conn.close()
     print(f"Job with ID {job_id} has been deleted.")
 
+def update_status():
+    print("\nCurrent Job Applications:")
+    view_all_jobs()
+    job_id = input("Enter the ID of the job you want to update: ")
+    new_status = input("Enter the new status (ex: Interviewing, Offer, Rejected): ")
+
+    conn = sqlite3.connect("jobs.db")
+    conn.execute("UPDATE jobs SET status = ? WHERE id = ?", (new_status, job_id))
+    conn.commit()
+    conn.close()
+
+    print(f"Job ID {job_id} updated to status '{new_status}'.\n")
+
 def main():
     initialize_db()
     while True:
         show_menu()
-        choice = input("Choose a option (1-4): ")
+        choice = input("Choose a option (1-5): ")
 
         if choice == '1':
             create_job()
@@ -67,6 +81,8 @@ def main():
         elif choice == '3':
             delete_job()
         elif choice == '4':
+            update_status()
+        elif choice == '5':
             break
         else:
             print("Invalid options. Please choose a valid option.")
